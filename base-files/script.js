01845efcd697fb0d5213914e14e9589525a1cd9a
Pavel Dobiáš <pavel23dob@gmail.com>
2022-07-07 00:07:09 +0200

    added theme functionality

--- base-files/script.js
+++ base-files/script.js
@@ -1,9 +1,38 @@
 console.log(window.location.href);
 
 screenSizeAlert();
+setThemeIcon(document.documentElement.dataset.theme);
 
 function screenSizeAlert() {
 	if (screen.width < 280) {
 		alert(`Your screen size is unsupported!\nYou might experience issues because width of your screen is less than 280px.\n\nScreen width = ${screen.width}px`);
+	}
+}
+
+document.querySelector('#topbar > .theme-toggle').addEventListener('pointerdown', toggleTheme);
+
+function toggleTheme(eventData) {
+    const rootDataset = document.documentElement.dataset;
+	let newTheme;
+    if (rootDataset.theme === 'light') {
+		newTheme = 'dark';
+    } else if (rootDataset.theme === 'dark') {
+		newTheme = 'light';
+    }
+
+	rootDataset.theme = newTheme;
+	setThemeIcon(newTheme);
+	localStorage.setItem('web-theme', newTheme);
+}
+
+function setThemeIcon(theme) {
+	const iconElement = document.querySelector('#topbar > .theme-toggle > img');
+	if (theme === 'dark') {
+		iconElement.src = '/sprites/images/moon-icon.png';
+		iconElement.alt = 'moon icon';
+	}
+	if (theme === 'light') {
+		iconElement.src = '/sprites/images/sun-icon.png';
+		iconElement.alt = 'sun icon';
 	}
 }
--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-document.querySelector('#topbar > .tri-dash-menu > .button').addEventListener('pointerdown', toggleLinkDropdown)
+document.querySelector('#topbar > .tri-dash-menu > .button').addEventListener('pointerdown', toggleLinkDropdown);
 
 function toggleLinkDropdown(eventData) {
     const menuElement = document.querySelector('#topbar > .tri-dash-menu');
     const iconElement = document.querySelector('#topbar > .tri-dash-menu > .button > img');
     if (menuElement.classList.contains('open')) {
         menuElement.classList.remove('open');
-        iconElement.src = "/sprites/images/tri-dash-icon.svg"
+        iconElement.src = "/sprites/images/tri-dash-icon.svg";
     } else {
         menuElement.classList.add('open');
-        iconElement.src = "/sprites/images/tri-dash-icon-open.svg"
+        iconElement.src = "/sprites/images/tri-dash-icon-open.svg";
     }
 }
